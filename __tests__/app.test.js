29d3a8f890e319050c0597ef91e61def57dd14d3
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-09 17:18:36 -0600

    create user test works

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,14 +24,19 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  
   it('creates a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
     const { email } = mockUser;
+
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
     });
   });
+
+
+
   afterAll(() => {
     pool.end();
   });
